81be73437f0a64914d8ff3f3402f83842bc5f3b9
promise Me <coinvest518@gmail.com>
2025-06-17 19:40:31 -0400

    air

---
+++ hooks/use-mobile.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+// Simple hook to detect if the device is mobile based on window width
+export function useIsMobile(breakpoint = 768): boolean {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth < breakpoint);
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [breakpoint]);
+
+  return isMobile;
+}
